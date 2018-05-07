277c8a0e214592e592a993a83b309e5b6cffc64e
iBug <stevepad@me.com>
2018-05-07 09:35:52 +0800

    Detach config from build script

---
+++ .travis/config.sh
@@ -0,0 +1,1 @@
+GH_REPO=iBug/iBug.github.io
--- .travis/prepare.sh
+++ .travis/prepare.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# Make the configuration
+. .travis/config.sh || true
 : ${GH_REPO:=iBug/iBug.github.io}
+
+set -e
 
 e_info() {
   echo -e "\x1B[36;1m[Info]\x1B[0m $*" >&2
@@ -18,5 +22,6 @@ e_error() {
   echo -e "\x1B[31;1m[Error]\x1B[0m $*" >&2
 }
 
-e_info "Cloning from GitHub"
+e_info "Cloning from GitHub:$GH_REPO.git"
 git clone "https://$GH_TOKEN@github.com/$GH_REPO.git" _site &>/dev/null
+e_success "Done"
