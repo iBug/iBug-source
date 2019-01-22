c35ef0cc7ec577676de59b15f2e7bdbae32632b0
iBug <7273074+iBug@users.noreply.github.com>
2019-01-22 11:35:03 +0800

    Generate cv.md from YAML

---
+++ cv.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/ruby
+
+require 'yaml'
+require 'time'
+
+front_matter = {
+  'title' => 'Curriculum Vitae',
+  'description' => "iBug's Timeline",
+  'layout' => "default",
+}
+
+data = YAML.load_file 'cv.yml'
+
+body = data['events'].sort.reverse.map do |date, text|
+  "<dt>#{date.strftime "%b %d, %Y"}<dt>\n<dd>\n#{text.strip}\n</dd>\n"
+end
+
+links = data['links'].sort.map do |k, v|
+  "  [#{k}]: #{v}"
+end
+
+File.open "cv.md", "w" do |f|
+  f.write "#{front_matter.to_yaml}---\n\n<dl>\n#{body.join "\n"}</dl>\n\n#{links.join "\n"}\n"
+end
