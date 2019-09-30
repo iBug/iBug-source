9ca9a46cdcd8c79d72399b75f8f943e38121ae3f
iBug <git@ibugone.com>
2019-09-30 17:40:11 +0800

    Convert better

--- _plugins/kramdown-images.rb
+++ _plugins/kramdown-images.rb
@@ -3,8 +3,11 @@ require 'kramdown/converter/html'
 module StandaloneImages
   def convert_p(el, indent)
     return super unless el.children.size == 1 && (el.children.first.type == :img || (el.children.first.type == :html_element && el.children.first.value == "img"))
-    el.children.first.attr["class"] ||= ""
-    el.children.first.attr["class"] += " block"
+    if el.children.first.attr["class"].nil?
+      el.children.first.attr["class"] = "block"
+    else
+      el.children.first.attr["class"] += " block"
+    end
     #convert(el, indent)
     super
   end
