0363963f37ef3646466ea954315eff5012acc8d1
iBug <git@ibugone.com>
2019-12-07 03:02:46 +0800

    Show commit SHA if CI build number is unavailable

--- script/deploy-circle.sh
+++ script/deploy-circle.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 COMMIT_MSG="Auto deploy from CircleCI build ${CIRCLE_BUILD_NUM:-?}"
-. ${0%/*}/deploy-ssh.sh
+# Currently using GitHub Actions for deployment
+#. ${0%/*}/deploy-ssh.sh
