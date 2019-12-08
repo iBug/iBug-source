c6da95c36378029663f06ebd31cfa4f1c2b618ad
iBug <git@ibugone.com>
2019-12-08 21:28:02 +0800

    Change GitHub Actions committer info

--- script/deploy-actions.sh
+++ script/deploy-actions.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
 
-COMMIT_MSG="Auto deploy from GitHub Actions"
-. ${0%/*}/deploy-ssh.sh
+. ${0%/*}/config.sh
+. ${0%/*}/util.sh
+: ${SRC:=_site} ${BRANCH:=master} ${COMMIT_MSG:="Auto deploy from GitHub Actions"}
+
+set -e
+
+if [ -z "${SSH_KEY_E}" ]; then
+  e_error "No SSH key present in environment, not pushing."
+  exit 1
+fi
+
+source_msg="$(git log -1 --pretty="[%h] %B")"
+
+pushd "$SRC" &>/dev/null
+e_info "Adding commit info"
+git config user.name "${GIT_USER:-GitHub}"
+git config user.email "${GIT_EMAIL:-noreply@github.com}"
+git add --all
+git commit --message "${COMMIT_MSG}" --message "$source_msg" &>/dev/null
+
+e_info "Pushing to GitHub"
+SSH_AUTH_SOCK=none \
+GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa" \
+git push origin ${BRANCH:-master}
+
+popd &>/dev/null
+e_success "Successfully deployed to GitHub Pages"
