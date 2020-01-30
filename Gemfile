781f66d86ed24b21effb981aa036f16b314cb482
iBug <git@ibugone.com>
2020-01-30 15:49:28 +0800

    Use mmistakes/jekyll-paginate-v2 as a workaround
    Ref: sverrirs/jekyll-paginate-v2#170

--- Gemfile
+++ Gemfile
@@ -37,6 +37,6 @@ group :jekyll_plugins do
   gem "jekyll-last-modified"
   gem "jekyll-assets"
   gem "jekyll-algolia"
-  gem "jekyll-archives" #, '= 2.2.1'
-  gem "jekyll-paginate-v2"
+  gem "jekyll-archives", '= 2.2.1'
+  gem "jekyll-paginate-v2", github: 'mmistakes/jekyll-paginate-v2', :branch => 'jekyll-v4'
 end
--- _plugins/liquid-bug-fix.rb
+++ _plugins/liquid-bug-fix.rb
@@ -3,37 +3,39 @@
 # https://github.com/jekyll/jekyll/pull/7967
 
 module Jekyll
-  class LiquidRenderer
-    class File
-      def render(*args)
-        reset_template_assigns
+  if VERSION[0] == '4'
+    class LiquidRenderer
+      class File
+        def render(*args)
+          reset_template_assigns
 
-        measure_time do
-          measure_bytes do
-            measure_counts do
-              @template.render(*args)
+          measure_time do
+            measure_bytes do
+              measure_counts do
+                @template.render(*args)
+              end
             end
           end
         end
-      end
 
-      # This method simply 'rethrows any error' before attempting to render the template.
-      def render!(*args)
-        reset_template_assigns
+        # This method simply 'rethrows any error' before attempting to render the template.
+        def render!(*args)
+          reset_template_assigns
 
-        measure_time do
-          measure_bytes do
-            measure_counts do
-              @template.render!(*args)
+          measure_time do
+            measure_bytes do
+              measure_counts do
+                @template.render!(*args)
+              end
             end
           end
         end
-      end
 
-      # clear assigns to `Liquid::Template` instance prior to rendering since
-      # `Liquid::Template` instances are cached in Jekyll 4.
-      def reset_template_assigns
-        @template.instance_assigns.clear
+        # clear assigns to `Liquid::Template` instance prior to rendering since
+        # `Liquid::Template` instances are cached in Jekyll 4.
+        def reset_template_assigns
+          @template.instance_assigns.clear
+        end
       end
     end
   end
