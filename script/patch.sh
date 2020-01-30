ea650742f19d3c8ef69296e2e5279bbe27ad9e99
iBug <git@ibugone.com>
2020-01-30 21:31:18 +0800

    Sort redirects.json by keys

--- script/patch.sh
+++ script/patch.sh
@@ -7,5 +7,9 @@ e_info "Patching generated site"
 
 touch "$SRC/.nojekyll"
 cat REMOTE_README.md > "$SRC/README.md"
+if type jq &>/dev/null; then
+  jq -S . < "$SRC/redirects.json" > /tmp/redirects.json
+  mv /tmp/redirects.json "$SRC/redirects.json"
+fi
 
 e_success "Patch complete"
