6af52fb1619df7c2a6c6c2d573793b5d298ba798
iBug <git@ibugone.com>
2020-05-23 02:03:57 +0800

    Use new number_of_words plugin
    Thanks to @ashmaroli's help:
    https://github.com/jekyll/jekyll/pull/7813#issuecomment-632553598

--- _plugins/number_of_words.rb
+++ _plugins/number_of_words.rb
@@ -1,8 +1,13 @@
-CJK_REGEX = /\p{Han}|\p{Katakana}|\p{Hiragana}|\p{Hangul}/
+CJK_CLASS = %!\p{Han}\p{Katakana}\p{Hiragana}\p{Hangul}!
+CJK_REGEX = %r![#{CJK_CLASS}]!o
+WORD_REGEX = %r![#{CJK_CLASS}\s]!o
+
 module Jekyll
   module Filters
     def number_of_words input
-      input.scan(CJK_REGEX).length + input.gsub(CJK_REGEX, ' ').split.length
+      cjk_count = input.scan(CJK_REGEX).length
+      return input.split.length if cjk_count.zero?
+      cjk_count + input.scan(WORD_REGEX).length
     end
   end
 end
