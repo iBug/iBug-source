5b3f4293b22d5b2f573f3574d378567c3713a288
iBug <git@ibugone.com>
2020-06-15 21:26:43 +0800

    Fix broken JS

--- script/build-netlify.sh
+++ script/build-netlify.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export CI=true LSI=true
 bundle exec jekyll build --profile --trace
 git clone --depth=1 --branch=master --single-branch https://github.com/iBug/image.git _site/image
 rm -rf _site/image/.git
