3f61090bff757cc959e10ad99105b0a5bcba19c5
iBug <git@ibugone.com>
2020-08-29 13:53:39 +0800

    Try again

--- _plugins/algolia-hook.rb
+++ _plugins/algolia-hook.rb
@@ -2,7 +2,7 @@ module Jekyll
   module Algolia
     module Hooks
       def self.before_indexing_each(record, node, context)
-        return nil if node.matches? 'git'
+        record.delete :git
         record
       end
     end
