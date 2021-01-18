053b2b03b8d256710629c958a51e9a2c678e4cf1
iBug <git@ibugone.com>
2021-01-19 00:06:01 +0800

    Fix util script again

--- script/util.sh
+++ script/util.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 e_info() {
-  printf "\x1B[36;1m[Info]\x1B[0m %s\n" "$*" >&2
+  printf "\033[36;1m[Info]\033[0m %s\n" "$*" >&2
 }
 
 e_success() {
-  printf "\x1B[32;1m[Success]\x1B[0m %s\n" "$*" >&2
+  printf "\033[32;1m[Success]\033[0m %s\n" "$*" >&2
 }
 
 e_warning() {
-  printf "\x1B[33;1m[Warning]\x1B[0m %s\n" "$*" >&2
+  printf "\033[33;1m[Warning]\033[0m %s\n" "$*" >&2
 }
 
 e_error() {
-  printf "\x1B[31;1m[Error]\x1B[0m %s\n" "$*" >&2
+  printf "\033[31;1m[Error]\033[0m %s\n" "$*" >&2
 }
