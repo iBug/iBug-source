1d9fabf5cef95e0221fc745d9d62dc97f93dadfd
iBug <git@ibugone.com>
2021-01-18 23:58:51 +0800

    Check libgsl-dev preinstalled on GH Actions

--- _plugins/tabindent.rb
+++ _plugins/tabindent.rb
@@ -1,8 +1,8 @@
 module HtmlBeautifier
   def self.beautify(html, options = {})
     options[:indent] = "\t"
-    "".tap { |output|
+    "".tap do |output|
       HtmlParser.new.scan html.to_s, Builder.new(output, options)
-    }
+    end
   end
 end
