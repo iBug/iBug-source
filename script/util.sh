edaaa9b7ca6d554edc9404d7dd02456f048cfe0f
iBug <git@ibugone.com>
2021-01-19 00:01:36 +0800

    Fix util script POSIX compat, drop apt install from GHA

--- script/util.sh
+++ script/util.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 e_info() {
-  echo -e "\x1B[36;1m[Info]\x1B[0m $*" >&2
+  printf "\x1B[36;1m[Info]\x1B[0m %s\n" "$*" >&2
 }
 
 e_success() {
-  echo -e "\x1B[32;1m[Success]\x1B[0m $*" >&2
+  printf "\x1B[32;1m[Success]\x1B[0m %s\n" "$*" >&2
 }
 
 e_warning() {
-  echo -e "\x1B[33;1m[Warning]\x1B[0m $*" >&2
+  printf "\x1B[33;1m[Warning]\x1B[0m %s\n" "$*" >&2
 }
 
 e_error() {
-  echo -e "\x1B[31;1m[Error]\x1B[0m $*" >&2
+  printf "\x1B[31;1m[Error]\x1B[0m %s\n" "$*" >&2
 }
