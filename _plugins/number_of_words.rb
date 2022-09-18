c048882b2bb92beb6011ea7f014aa0ba9bb48426
iBug <git@ibugone.com>
2022-09-18 21:33:58 +0800

    Change number_of_words plugin

--- _plugins/number_of_words.rb
+++ _plugins/number_of_words.rb
@@ -1,9 +1,9 @@
-CJK_CLASS = '\p{Han}\p{Katakana}\p{Hiragana}\p{Hangul}'
-CJK_REGEX = %r![#{CJK_CLASS}]!o
-WORD_REGEX = %r![^#{CJK_CLASS}\s]+!o
-
 module Jekyll
   module Filters
+    CJK_CLASS = '\p{Han}\p{Katakana}\p{Hiragana}\p{Hangul}'
+    CJK_REGEX = %r![#{CJK_CLASS}]!o
+    WORD_REGEX = %r![^#{CJK_CLASS}\s]+!o
+
     def number_of_words(input)
       cjk_count = input.scan(CJK_REGEX).length
       return input.split.length if cjk_count.zero?
